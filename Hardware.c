5211ab4b709a0965f326c0ebf982db5ae4cb38d2
Daniel <danyallwy280@gmail.com>
2025-07-23 13:32:19 +0330

    Ready Protype

--- Hardware.c
+++ Hardware.c
@@ -1,12 +1,16 @@
 #include "stdio.h"
 #include "stdint.h"
 
+//  This simple structure, is a simple Memoty of a computer
+//  Containing 4 Register and 1 RAM
 typedef struct
 {
-    //  Save number in range [0,255] into registers
+    //  Save 8 bit number in range [0,255] into registers
     uint8_t R1;
     uint8_t R2;
     uint8_t R3;
     uint8_t R4;
+
+    //  Rasarving ~64kb Ram
     uint8_t ram[65536];
 } Register;
